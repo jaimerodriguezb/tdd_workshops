b62488674d9b7ed9d46d592414907e1e1212cd00
Jaime Rodriguez <jaimerodriguez@gmail.com>
2023-08-08 16:34:33 -0500

    Calcular entrada - REFACTOR

--- perceptron/perceptron_simple.py
+++ perceptron/perceptron_simple.py
@@ -1,20 +1,19 @@
 import unittest
+import numpy as np
 
-def calcular_entrada(x1,x2,x3,w1,w2,w3):
-    return x1*w1 + x2*w2 + x3*w3
+def calcular_entrada(x,w):
+    return np.dot(x,w)
+   
 
 class TestPerceptron(unittest.TestCase):
-    
+   
     def test_calcular_entrada(self):
-        x1 = 1
-        x2 = 2
-        x3 = 3
-        w1 = 0.1
-        w2 = 0.2
-        w3 = 0.3
+        x = [1,2,3]
+        w = [0.1,0.2,0.3]
         y1 = None
-        y1 = calcular_entrada(x1,x2,x3,w1,w2,w3)
-        
+       
+        y1 = calcular_entrada(x,w)
+       
         self.assertEqual(y1,1.4)
 
 
