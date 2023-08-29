94084726dfdb455a38194e2c890d9ba05728a668
Jaime Rodriguez <jaimerodriguez@gmail.com>
2023-08-29 17:13:17 -0500

    RED - BoomBarrier, plate ok and payment FAIL

--- horno/parking/boombarrier.py
+++ horno/parking/boombarrier.py
@@ -13,5 +13,12 @@ class BoomBarrierTest(unittest.TestCase):
 
         self.assertEqual(result, 'LIFT')
 
+    def test_carplate_ok_payment_fail(self):
+        plate = 'abc123'
+        payment = None
+
+        result = validate_payment(plate, payment)
+
+        self.assertEqual(result, 'NO PAYMENT')
 
 unittest.main(__name__)
